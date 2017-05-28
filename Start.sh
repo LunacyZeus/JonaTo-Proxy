5a7b816925726f66704d20cfc18ca2923816c257
LunacyZeus <LunacyZeus@users.noreply.github.com>
2017-05-28 13:46:34 +0800

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget -N https://github.com/LunacyZeus/JonaTo-Proxy/raw/master/fssh.sh
-wget -N https://github.com/LunacyZeus/JonaTo-Proxy/blob/master/ip_list_servers.txt
-wget -N https://github.com/LunacyZeus/JonaTo-Proxy/blob/master/remote_cmd.txt
-wget -N https://github.com/LunacyZeus/JonaTo-Proxy/blob/master/userpass.sh
+wget --no-check-certificate -N https://github.com/LunacyZeus/JonaTo-Proxy/raw/master/fssh.sh
+wget --no-check-certificate -N https://github.com/LunacyZeus/JonaTo-Proxy/blob/master/ip_list_servers.txt
+wget --no-check-certificate -N https://github.com/LunacyZeus/JonaTo-Proxy/blob/master/remote_cmd.txt
+wget --no-check-certificate -N https://github.com/LunacyZeus/JonaTo-Proxy/blob/master/userpass.sh
 
 echo "修改userpass.sh是设置登录的服务器用户名和密码"
 echo "修改ip_list_servers.txt是要登录的服务器"
